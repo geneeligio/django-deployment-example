9fc8623c78475902c42eced8bf55c9a167b36b08
unknown <geneeligio@gmail.com>
2020-04-11 09:33:04 +0800

    'first'

--- learning_templates/basic_app/urls.py
+++ learning_templates/basic_app/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
-from . import views
+from django.conf.urls import url
+from basic_app import views
 
-# SET THE NAMESPACE!
-app_name = 'basic_app'
+# template tagging
+app_name  = 'basic_app'
 
 urlpatterns=[
-    path('relative/',views.relative,name='relative'),
-    path('other/',views.other,name='other'),
+    url(r'^relative/$',views.relative,name='relative'),
+    url(r'^other/$',views.other,name='other')
 ]
--- learning_templates/learning_templates/urls.py
+++ learning_templates/learning_templates/urls.py
@@ -1,7 +1,7 @@
 """learning_templates URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path, include
 from basic_app import views
 
+
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('admin/', admin.site.urls),
-    path('basic_app/',include('basic_app.urls')),
+    url(r'^$',views.index,name='index'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^basic_app/',include('basic_app.urls'))
 ]
